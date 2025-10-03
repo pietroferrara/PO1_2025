88bc65ed24b9ed73034a261b6c97a002f3d465a2
Pietro Ferrara <pietro.ferrara@gmail.com>
2025-10-03 15:11:53 +0200

    lecture 3: bytecode

--- src/Runner.java
+++ src/Runner.java
@@ -1,17 +1,18 @@
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        int x = fattoriale(10);
-        System.out.println(x);
+        int x = 0;
+        int j = 12;
+        if(x < 0)
+            j = -12;
     }
 
-    static int fattoriale(int i) {
-        if (i == 1) {
-            return 1;
-        } else {
-            return i * fattoriale(i-1);
+    static int sum(int[] array) {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
+        return sum;
     }
 
 }
